7fda69cbac48d0ba6c32acdb027bdf3caee5591e
Brian Bui <brianbui012@gmail.com>
2020-07-27 19:34:16 -0700

    Corrected nav bar

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,14 +1,7 @@
-const navToggle = document.querySelector('.nav-toggle');
-const navLinks = document.querySelectorAll('.nav__link');
-
-
-navToggle.addEventListener('click', () => {
-    document.body.classList.toggle('nav-open');
-});
-
+const navToggle = document.querySelector('#toggle');
 
 document.addEventListener("click", (event) => {
     if (!navToggle.contains(event.target)) {
-        document.body.classList.remove('nav-open');
+        navToggle.checked = false;
     }
 });
